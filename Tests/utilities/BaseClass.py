882cff5aca614fc7e34d62c4d81bc00017a2d6f7
Aryavart <mailaryavart@gmail.com>
2024-12-08 17:44:24 +0530

    updated

--- Tests/utilities/BaseClass.py
+++ Tests/utilities/BaseClass.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 
-
+# Using base class to create custom utilities
 @pytest.mark.usefixtures("setup")
 class BaseClass:
 
@@ -26,6 +26,7 @@ class BaseClass:
     def verifyLinkPresence(self, text):
         element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, text)))
 
+    # Adding explicit wait
     def selectOptionByText(self, locator, text):
         sel = Select(locator)
         sel.select_by_visible_text(text)
